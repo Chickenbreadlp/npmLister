9be1bc93ea7c3928a471f4cb88d726931418a04e
Chickenbread <Chickenbreadlp@users.noreply.github.com>
2019-07-23 08:58:35 +0200

    Patched npx execution for good

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const fs = require('fs');
 const path = require('path');
 const getDirectories = source => fs.readdirSync(source, { withFileTypes: true }).filter(dirent => dirent.isDirectory()).map(dirent => dirent.name)
